e9cca4f9a86f3d6069b0a3d2da38c2cb6283fef5
Philip Karlsson <philipkarlsson@me.com>
2018-12-05 11:03:50 +0100

    Added utility to make a recursive find and replace

--- rplugin/python3/utils.py
+++ rplugin/python3/utils.py
@@ -5,12 +5,24 @@
 # ============================================================================
 
 import neovim
+import os
+
+def python_input(nvim, message = 'input'):
+    nvim.command('call inputsave()')
+    nvim.command("let user_input = input('" + message + ": ')")
+    nvim.command('call inputrestore()')
+    return nvim.eval('user_input')
 
 
 @neovim.plugin
 class UtilsPlug(object):
     def __init__(self, nvim):
         self.nvim = nvim
+        self.logstr = []
+        self.logstr.append('== utils debug ==')
+
+    def log(self, s):
+        self.logstr.append(s)
 
     def doRg(self, pattern, word):
         if len(word) == 0:
@@ -44,3 +56,26 @@ class UtilsPlug(object):
     def rgcpp(self, args, range):
         pattern = '--no-ignore --type cpp'
         self.doRg(pattern, args)
+
+    @neovim.command("UtilsShowLog", range='', nargs='*', sync=True)
+    def usl(self, args, range):
+        self.nvim.command('e utils_log')
+        self.nvim.command('setlocal buftype=nofile')
+        self.nvim.command('setlocal filetype=utils_log')
+        self.nvim.current.buffer.append(self.logstr)
+
+    @neovim.command("UtilsFindReplace", range='', nargs='*', sync=True)
+    # TODO respect .gitignore etc.
+    def frc(self, args, range):
+        currentSymbol = self.nvim.eval("expand('<cword>')")
+        # Prompt what to replace this symbol with
+        resp = python_input(self.nvim, message="Replace " + currentSymbol + " with in cwd?")
+        if resp == "":
+            return
+        cmd = "find . -type f ! -name \"*.o\" ! -name \"*.git\" -print0 | xargs -0 sed -i '' -e 's/%s/%s/g'" % (currentSymbol, resp)
+        self.log(resp)
+        os.system(cmd)
+        self.nvim.command('set noconfirm')
+        self.nvim.command('bufdo e')
+        self.nvim.command('set confirm')
+
